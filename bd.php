f968ba7f644283e9605f124710cbf16841b74659
FernandoArreguinCepeda <fernando.arreguin.24s@utzmg.edu.mx>
2025-11-28 17:29:56 -0600

    Solucion error

--- bd.php
+++ bd.php
@@ -1,17 +1,22 @@
 <?php
 function conectarBD() {
 
-    $host = getenv('DB_HOST');
-    $dbname = getenv('DB_NAME');
-    $user = getenv('DB_USER');
-    $password = getenv('DB_PASSWORD');
+    $host = getenv('DB_HOST') ?: 'db'; 
+    $dbname = getenv('DB_NAME') ?: 'ItemTerraria';
+    $user = getenv('DB_USER') ?: 'postgres';
+    $password = getenv('DB_PASSWORD') ?: '9894';
 
     $conn_string = "host=$host port=5432 dbname=$dbname user=$user password=$password";
     
-    $conexion = pg_connect($conn_string);
+
+    $conexion = @pg_connect($conn_string); 
 
     if (!$conexion) {
         exit("Error de conexión: " . pg_last_error());
     }
     return $conexion;
 }
+function seleccionar($conexion, $query, $datos) {
+    return pg_query_params($conexion, $query, $datos);
+}
+?>
