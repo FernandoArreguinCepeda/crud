bd546c8e48d2058fff4a98826ab16c0089bb5d68
FernandoArreguinCepeda <fernando.arreguin.24s@utzmg.edu.mx>
2025-11-28 17:22:02 -0600

    Docker Base datos

--- bd.php
+++ bd.php
@@ -1,11 +1,17 @@
 <?php
 function conectarBD() {
-    $conexion = pg_connect("host=localhost port=5432 dbname=ItemTerraria user=postgres password=9894");
+
+    $host = getenv('DB_HOST') ?: 'db_name_o_ip';
+    $dbname = getenv('DB_NAME') ?: 'ItemTerraria';
+    $user = getenv('DB_USER') ?: 'postgres';
+    $password = getenv('DB_PASSWORD') ?: '9894';
+
+    $conn_string = "host=$host port=5432 dbname=$dbname user=$user password=$password";
+    
+    $conexion = pg_connect($conn_string);
+
     if (!$conexion) {
         exit("Error de conexión: " . pg_last_error());
     }
     return $conexion;
-}
-function seleccionar($conexion, $query, $datos) {
-    return pg_query_params($conexion, $query, $datos);
 }
