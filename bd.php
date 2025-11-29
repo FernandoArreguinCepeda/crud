2f57b0426952ba9617ca625a3f945a7cc9f76f97
FernandoArreguinCepeda <fernando.arreguin.24s@utzmg.edu.mx>
2025-11-28 18:32:57 -0600

    Intentode solucionar base de datos 5

--- bd.php
+++ bd.php
@@ -1,14 +1,25 @@
 <?php
-
+// LA FUNCIÓN CONECTARBD ES OBLIGATORIA
+function conectarBD() {
 
     $host = getenv('DB_HOST') ?: 'dpg-d4l3ad7pm1nc738jacc0-a'; 
-    $dbname = getenv('DB_NAME') ?: 'itemterraria';
-    $user = getenv('DB_USER') ?: 'itemterraria_user';
-    $password = getenv('DB_PASSWORD') ?: 'W7eaBZNXymyjgWfj35vb8xawh3aSJ7bD';
+    $dbname = getenv('DB_NAME') ?: 'itemterraria'; 
+    $user = getenv('DB_USER') ?: 'itemterraria_user'; 
+    $password = getenv('DB_PASSWORD') ?: 'W7eaBZNXymyjgWfj35vb8xawh3aSJ7bD'; 
+    $conn_string = "host=$host port=5432 dbname=$dbname user=$user password=$password";
+    
+    // Intenta conectar
+    $conexion = @pg_connect($conn_string); 
 
-$conn_string = "host=$host port=5432 dbname=$dbname user=$user password=$password";
-$conexion = pg_connect($conn_string); 
-if (!$conexion) {
-    die("Error de conexión con la base de datos: " . pg_last_error());
+    if (!$conexion) {
+
+        die("Error de conexión a la base de datos. Host: " . $host . ". Verifica las 4 variables de entorno en Render.");
+    }
+    return $conexion;
+}
+
+
+function seleccionar($conexion, $query, $datos) {
+    return pg_query_params($conexion, $query, $datos);
 }
 ?>
